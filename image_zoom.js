04014f001f0cc11989b62dd9d60dfeabfae8cedc
Nancy Zhang <nancy.zhang143@gmail.com>
2023-11-18 07:33:23 -0500

    add favicon, try to fix line color

--- image_zoom.js
+++ image_zoom.js
@@ -2,18 +2,25 @@ window.onload = function() {
   const imgs = document.querySelectorAll('.gallery img');
   const fullPage = document.querySelector('#fullpage');
   const bigImg = document.querySelector('#bigimg');
+  const body = document.querySelector('body');
 
   imgs.forEach(img => {
     img.addEventListener('click', function() {
       fullPage.style.display = 'flex';
       bigImg.setAttribute('src', img.getAttribute('src'));
+      body.style.height = "100%";
+      body.style.overflowY = "hidden";
     });
   });
 
   bigImg.addEventListener('click', function() {
     fullPage.style.display = 'none';
+    body.style.height = "";
+    body.style.overflowY = "";
   })
   fullPage.addEventListener('click', function() {
     fullPage.style.display = 'none';
+    body.style.height = "";
+    body.style.overflowY = "";
   })
 }
