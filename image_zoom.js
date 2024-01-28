6f49966b9167ef83a57dcfea97bc11ec25575c73
Nancy Zhang <nancy.zhang143@gmail.com>
2024-01-28 14:03:19 -0500

    update image expansion

--- image_zoom.js
+++ image_zoom.js
@@ -1,29 +1,12 @@
 window.onload = function() {
-  const imgs = document.querySelectorAll('.gallery img');
-  const fullPage = document.querySelector('#fullpage');
-  const bigImg = document.querySelector('#bigimg');
-  const body = document.querySelector('body');
+  const imgDivs = document.querySelectorAll('.gallery div');
 
-  imgs.forEach(img => {
-    img.addEventListener('click', function() {
-      fullPage.style.display = 'flex';
-      bigImg.setAttribute('src', img.getAttribute('src'));
-      body.style.height = "100%";
-      body.style.overflow = "hidden";
+  imgDivs.forEach(div => {
+    div.addEventListener('click', function() {
+		div.classList.toggle("fullgriditem")
+		if (div.classList.contains("fullgriditem")) {
+		  div.scrollIntoView({ behavior: "auto", block: "center", inline: "nearest" });
+		}
     });
-  });
-
-  bigImg.addEventListener('click', function() {
-    fullPage.style.display = 'none';
-    body.style.height = "";
-    body.style.overflow = "";
-    bigImg.setAttribute('src', "");
-  });
-
-  fullPage.addEventListener('click', function() {
-    fullPage.style.display = 'none';
-    body.style.height = "";
-    body.style.overflow = "";
-    bigImg.setAttribute('src', "");
   });
 }
